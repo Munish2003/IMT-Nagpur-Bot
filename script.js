4f2b6afb5b66d46be9686497f4ea61371b35bf86
Munish Kumar <135483933+Munish2003@users.noreply.github.com>
2025-11-06 17:22:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -207,7 +207,7 @@ async function typewriterEffect(element, text, speed = 20) {
 // ============================
 
 // const N8N_WEBHOOK_URL_LEFT = 'https://munishkumar.app.n8n.cloud/webhook/a117424e-1eb5-46cf-bfe3-fabd6bd03c79/chat';
-const N8N_WEBHOOK_URL_LEFT = 'https://munishkumarr.app.n8n.cloud/webhook/6125090e-1bff-4b61-ba78-2e04e96ca1e5/chat';
+const N8N_WEBHOOK_URL_LEFT = 'https://zoximan8n.app.n8n.cloud/webhook/6125090e-1bff-4b61-ba78-2e04e96ca1e5/chat';
 
 const chatToggleBtnLeft = document.getElementById('chatToggleBtnLeft');
 const chatWindowLeft = document.getElementById('chatWindowLeft');
@@ -599,5 +599,6 @@ document.addEventListener('keydown', (e) => {
 });
 
 console.log('✅ IMT Nagpur Website Loaded Successfully!');
+
 
 
